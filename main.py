4a2a2ed8495a31cbfbcf7331a5a72980045dceec
Ridwan Simjee <simjeer@uci.edu>
2023-08-05 02:54:36 -0700

    Information Retrieval and Filtering
    Program can make an api call to get course info or take from locally
    stored json file. The course info is filtered to just compsci courses
    with unnecessary info removed.

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-def main():
-    None
+import requests
+import json
+def make_api_call() -> list:
+    """Makes the api call to peterportal for the current list of all courses and returns the list of dictionaries"""
+    response = requests.get("https://api.peterportal.org/rest/v0/courses/all")
+    return response.json()
 
-if __name__ = "__main__":
+def grab_local_file() -> list:
+    """Gets the local .txt file in the same format as the api call (to not strain the api while building this application)"""
+    with open('all_courses.json', 'r', encoding = 'utf-8') as local_file:
+        text = json.loads(local_file.read())
+    return text
+
+
+def finding_compsci_courses(courses: list) -> list:
+    """Takes out compsci courses from all course list, removes unnecessary information, and returns a new list of filtered dictionaries """
+    compsci_courses = []
+    for course in courses:
+        if course["department"] == "COMPSCI" or course["department"] == "I&C SCI" and course["course_level"] == "Upper Division (100-199)":
+            del course['course_level'], course['professor_history'], course['terms'], course['id'], course['department_alias'], course['school'], course['department_name']
+            compsci_courses.append(course)
+    return compsci_courses
+
+def main():
+    for course in finding_compsci_courses(grab_local_file()):
+        print(course)
+
+if __name__ == "__main__":
     main()
