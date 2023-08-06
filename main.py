16308a79cad1e7973ee21110e236c2b09640ee16
Ridwan Simjee <simjeer@uci.edu>
2023-08-06 02:17:28 -0700

    Prerequisite groundwork
    Courses are now held in a dictionary. Program asks user to select which
    prerequisites, keeping track of all prerequisites and the user's
    prerequisites.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+
+prereq_set = set()
 def make_api_call() -> list:
     """Makes the api call to peterportal for the current list of all courses and returns the list of dictionaries"""
     response = requests.get("https://api.peterportal.org/rest/v0/courses/all")
@@ -14,16 +16,38 @@ def grab_local_file() -> list:
 
 def finding_compsci_courses(courses: list) -> list:
     """Takes out compsci courses from all course list, removes unnecessary information, and returns a new list of filtered dictionaries """
-    compsci_courses = []
+    compsci_courses = dict()
     for course in courses:
-        if course["department"] == "COMPSCI" or course["department"] == "I&C SCI" and course["course_level"] == "Upper Division (100-199)":
+        if (course["department"] == "COMPSCI" or course["department"] == "I&C SCI") and course["course_level"] == "Upper Division (100-199)":
             del course['course_level'], course['professor_history'], course['terms'], course['id'], course['department_alias'], course['school'], course['department_name']
-            compsci_courses.append(course)
+            compsci_courses[course['department'] + ' ' + course['number']] = course
+            prereq_set.update(course['prerequisite_list'])
     return compsci_courses
 
+def prereqs() -> None:
+    user_prereqs = set()
+    print(prereq_set)
+    print("\nSelect the classes that you have already taken, seperated by commas. (in the form: CSE 1,I&C SCI 2,COMPSCI 3, etc..)")
+    prereq_input = input().upper()
+    while prereq_input != "F":
+        for inputted_prereq in prereq_input.split(','):
+            inputted_prereq = inputted_prereq.strip()
+            if not inputted_prereq in prereq_set:
+                print("\n"+inputted_prereq + " was not in the prerequisite list and was not added.")
+            else:
+                user_prereqs.add(inputted_prereq)
+        print("\nChosen classes:")
+        print(user_prereqs)
+        print("\nNon-chosen classes:")
+        print(prereq_set - user_prereqs)
+        print("\nIs that all? Choose more completed courses or type \"f\" when finished.")
+        prereq_input = input().upper()
+
 def main():
-    for course in finding_compsci_courses(grab_local_file()):
-        print(course)
+#    for course in finding_compsci_courses(grab_local_file()).items():
+#        print(course)
+    finding_compsci_courses(grab_local_file())
+    prereqs()
 
 if __name__ == "__main__":
     main()
