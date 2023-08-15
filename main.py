5e1106e8b489a9219e0aa0938f4364ba1df4777c
Ridwan Simjee <simjeer@uci.edu>
2023-08-15 00:09:18 -0700

    Course Suggestions and Prereq Bug Fix
    Course suggestion method can now use the recursive method to offer
    course suggestions. A bug was fixed, where methods were checking against
    the pre_req list instead of the user_prereqs when offering courses. The
    pre_req list is no longer global, the method use has been changed, and
    tests have been updated.

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import scraping
 import json
 
-prereq_set = set()
+user_prereqs = set()
 
-
-def filtering_compsci_courses(courses: list) -> list:
-    """Takes out compsci courses from all course list, removes unnecessary information, and returns a new list of filtered dictionaries """
+def filtering_compsci_courses(courses: list) -> (dict, set):
+    """Takes out compsci courses from all course list, removes unnecessary information, and returns a new dictionary of filtered dictionaries """
     compsci_courses = dict()
+    prereq_set = set()
     for course in courses:
         if (course["department"] == "COMPSCI" or course["department"] == "I&C SCI") and course["course_level"] == "Upper Division (100-199)":
             del course['course_level'], course['professor_history'], course['terms'], course['id'], course['department_alias'], course['school'], course['department_name']
             compsci_courses[course['department'] + ' ' + course['number']] = course
             prereq_set.update(course['prerequisite_list'])
-    return compsci_courses
+    return compsci_courses, prereq_set
 
-def prereqs() -> None:
+def prereqs(prereq_set: set) -> None:
     """Collects the information on what courses the user has already taken. Still needs ability to redact a course."""
-    user_prereqs = set()
+    #user_prereqs = set()
     print(sorted(prereq_set))
     print("\nSelect the classes that you have already taken, seperated by commas. (in the form: CSE 1,I&C SCI 2,COMPSCI 3, etc..)")
     prereq_input = input().upper()
@@ -34,52 +34,38 @@ def prereqs() -> None:
         print("\nIs that all? Choose more completed courses or type \"f\" when finished.")
         prereq_input = input().upper()
 
-def offer_course_suggestions(prereq_tree) -> None:
-    if prereq_tree.strip() == '': return True
-    else: recursive_prerequisite_breakdown(json.loads(prereq_tree))
-
-def recursive_prerequisite_breakdown(prereq_tree: dict|bool) -> bool:
+def recursive_prerequisite_breakdown(prereq_tree: dict) -> bool:
     if 'AND' in prereq_tree:
         for branch in prereq_tree['AND']:
             if type(branch) == dict:
                 branch = recursive_prerequisite_breakdown(branch)
-            if (type(branch) == str and branch not in prereq_set) or (type(branch) == bool and not branch):
+            if (type(branch) == str and branch not in user_prereqs) or (type(branch) == bool and not branch):
                 return False
         return True
     elif 'OR' in prereq_tree:
         for branch in prereq_tree['OR']:
             if type(branch) == dict:
                 branch = recursive_prerequisite_breakdown(branch)
-            if (type(branch) == str and branch in prereq_set) or (type(branch) == bool and branch):
+            if (type(branch) == str and branch in user_prereqs) or (type(branch) == bool and branch):
                 return True
         return False
 
-
-    """
-    if prereq_tree.strip() == '':
-        return True
-    else: #{"AND": ["I&C SCI 33", "I&C SCI 61", {"OR": ["MATH 3A", "I&C SCI 6N"]}]}
-        prereq_tree = prereq_tree.split('{') #['"AND": ["I&C SCI 33", "I&C SCI 61",' , '"OR": ["MATH 3A", "I&C SCI 6N"]}]}']
-    current = prereq_tree[len(prereq_tree)-1] # '"OR": ["MATH 3A", "I&C SCI 6N"]}]}'
-    elements = current[current.index('[') + 1:current.index(']')].split(',')
-    if current.startswith("\"OR\""):
-        for element in elements:
-            if element.strip().strip('\"') in prereq_set:
-                return True
-        return False
-    elif current.startswith("\"AND\""):
-        for element in elements:
-            if element.strip().strip('\"') not in prereq_set:
-                return False
-        return True
-    """
+def offer_course_suggestions(courses: dict) -> list:
+    possible_courses = list()
+    for course in courses.values():
+        if course['prerequisite_tree'].strip() == '':
+            possible_courses.append(course)
+        elif recursive_prerequisite_breakdown(json.loads(course['prerequisite_tree'])):
+            possible_courses.append(course)
+    return possible_courses
 
 
 def main():
-#    for course in filtering_compsci_courses(grab_local_file()):
-#       print(course)
-    offer_course_suggestions(filtering_compsci_courses(grab_local_file()))
- #   prereqs()
+    scrape, prereq_set = filtering_compsci_courses(scraping.grab_local_file())
+    prereqs(prereq_set)
+    for suggestion in offer_course_suggestions(scrape):
+        print(suggestion)
+
 
 if __name__ == "__main__":
     main()
